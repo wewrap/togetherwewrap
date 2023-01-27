85b2d8d5eb7326e933555ed2df902f706f53b7f5
kevin <kevinvong0129@gmail.com>
2023-01-27 13:26:13 -0700

    create http get '/' hello world

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello World");
+})
+
 const port = 8000;
 
 app.listen(8000, () => {
-    console.log(`Server started: http://localhost:8000}/`);
+    console.log(`Server started: http://localhost:${port}/`);
 }); 
