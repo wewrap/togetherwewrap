cc5653bacffd4e03cd2dc5ee7b08ff31b6069b4c
kevin <kevinvong0129@gmail.com>
2023-01-27 14:10:10 -0700

    add a button to display data received from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
+import axios from "axios";
+import { useState } from "react";
+const serverURL = 'http://localhost:8000'
+
 function App() {
+  const [message, setMessage] = useState("");
   
+  const handleClick = async () => {
+    if (message) {
+      setMessage("")
+      return
+    }
+    const res = await axios.get(`${serverURL}/api`)
+    const serverMessage = res.data.data
+    setMessage(serverMessage)
+  }
   return (
     <div className="App">
-      <button>button</button>
+      <h2>{message ? message : ""}</h2>
+      <button onClick={handleClick}>button</button>
     </div>
   );
 }
