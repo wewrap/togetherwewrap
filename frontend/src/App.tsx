afc569afee931481c563c1617b3959fcd05d8c55
kevin <kevinvong0129@gmail.com>
2023-01-29 10:11:09 -0700

    refactor App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,32 @@ import { useState } from "react";
 const serverURL = 'http://localhost:8000'
 
 function App() {
-  const [message, setMessage] = useState("");
-  
+  const [message, setMessage] = useState<string>("");
+
+  const resetMessage = () => {
+    setMessage("")
+  }
+
   const handleClick = async () => {
-    if (message) {
-      setMessage("")
+    const displayingMessage = message ? true : false
+    if (displayingMessage) {
+      resetMessage()
       return
+    };
+
+    try {
+      const res = await axios.get(`${serverURL}/api`)
+      const serverMessage = res.data.data
+      setMessage(serverMessage)
+    } catch (error) {
+      //error has type 'unknown'. My fix: assert error as any.
+      console.error("Error connecting to backend:", (error as any).message)
+      alert(`Error connecting to backend: ${(error as any).message}`)
     }
-    const res = await axios.get(`${serverURL}/api`)
-    const serverMessage = res.data.data
-    setMessage(serverMessage)
   }
   return (
     <div className="App">
-      <h2>{message ? message : ""}</h2>
+      <h2>{message ?? ""}</h2>
       <button onClick={handleClick}>click me!</button>
     </div>
   );
