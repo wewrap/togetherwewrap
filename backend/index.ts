0e237d3a46f35d3704c3776e6da73a5d00207e37
kdata12 <106122976+kdata12@users.noreply.github.com>
2023-01-30 19:40:34 -0700

    Connect server client (#1)
    * create http get '/' hello world
    
    * change start script to 'node index.js'
    
    * install express, add 'dev' and 'tsc' to scripts
    
    * add GET /api endpoint
    
    * remove all react boilerplate files except for app.tsx and index.tsx
    
    * remove unnecessary imports
    
    * add axios dependency
    
    * add a button to display data received from backend
    
    * change button to 'click me!'
    
    * refactor App component
    
    * refactor isDisplayMessage

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello World");
+});
+
+app.get('/api', (req, res) => {
+    res.status(200).json({
+        data: "Together we wrap!"
+    })
+})
+
 const port = 8000;
 
 app.listen(8000, () => {
-    console.log(`Server started: http://localhost:8000}/`);
+    console.log(`Server started: http://localhost:${port}/`);
 }); 
