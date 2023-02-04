10230a9aaa06c7b85610f030e0bbd7485f20f375
nathan <youaremeandthem@gmail.com>
2023-02-04 02:04:51 -0500

    stash

--- backend/index.ts
+++ backend/index.ts
@@ -5,3 +5,24 @@ const port = 8000;
 app.listen(8000, () => {
     console.log(`Server started: http://localhost:8000}/`);
 }); 
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
