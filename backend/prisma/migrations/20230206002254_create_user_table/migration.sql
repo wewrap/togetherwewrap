38c13c7c0a26b7fdc9aa57ed6e328e40e7e6a8e7
jesshe004 <101468113+jesshe004@users.noreply.github.com>
2023-02-05 19:25:59 -0500

    ref-xied users table

--- backend/prisma/migrations/20230206002254_create_user_table/migration.sql
+++ backend/prisma/migrations/20230206002254_create_user_table/migration.sql
@@ -4,11 +4,9 @@ CREATE TABLE `User` (
     `email` VARCHAR(191) NOT NULL,
     `firstName` VARCHAR(191) NOT NULL,
     `lastName` VARCHAR(191) NOT NULL,
+    `passwords` VARCHAR(191) NOT NULL,
     `birthdate` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
-    UNIQUE INDEX `User_firstName_key`(`firstName`),
-    UNIQUE INDEX `User_lastName_key`(`lastName`),
-    UNIQUE INDEX `User_birthdate_key`(`birthdate`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
