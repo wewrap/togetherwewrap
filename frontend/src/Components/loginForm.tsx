fd2d1c1f9e9a7aeb13e7f87a1785c421a5e02033
Fahad Khan <fakha@MacBook-Pro-2.attlocal.net>
2023-02-12 15:16:51 -0600

    fixed some word differences. Keeping underscores only between multiple words

--- frontend/src/Components/loginForm.tsx
+++ frontend/src/Components/loginForm.tsx
@@ -29,6 +29,7 @@ export const LoginForm = () => {
 
     return (
         <section>
+          {/* TODO (Fahad) T78: refactor this code after wewrap logo can be downloaded */}
           <img src='./logo192.png' alt='placeholder for wewrap logo'></img>
           <h1>Log in to your account</h1>
           <p className='subheader'>Don't have an account? Sign up</p>
@@ -37,12 +38,12 @@ export const LoginForm = () => {
               <div>
                   <p className='form_text'>username*</p>
                   <label htmlFor="username"></label>
-                  <input id="username" name="username" type="text" autoComplete="username" required value={username} onChange={event => handleUsernameChange(event)}/>
+                  <input id="username" name="username" type="text" autoComplete="on" required value={username} onChange={handleUsernameChange}/>
               </div>
               <div>
                   <p className='form_text'>password*</p>
-                  <label htmlFor="current-password"></label>
-                  <input id="current-password" name="password" type="password" autoComplete="current-password" required value={password} onChange={event => handlePasswordChange(event)}/>
+                  <label htmlFor="current_password"></label>
+                  <input id="current_password" name="password" type="password" autoComplete="on" required value={password} onChange={handlePasswordChange}/>
                   <p className='form_text'>remember me</p>
               </div>
               <div>
