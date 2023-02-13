9a2f404146ac0e7426fa77f85bd7d865b00d2f1d
jesshe004 <101468113+jesshe004@users.noreply.github.com>
2023-02-13 08:20:58 -0500

    colors.tsx

---
+++ frontend/src/colors.tsx
@@ -0,0 +1,23 @@
+/**
+ * Wewrap color pallete. 
+ *  
+ */
+
+export const ALL_COLORS = {
+    //neutrals 
+    white: 'rgb(255,255,255)',
+    dark: 'rgb(51,51,51', 
+
+    //green
+    carribeanGreen: 'rgb(6,214,160)',
+
+    //blue 
+    blueMunsell: 'rgb(27,154,170)',
+
+    //pink
+    paradisePink: 'rgb(239,71,111',
+
+    //yellow
+    lightYellow: 'rgb(248,255,229)',
+    orangeYellow: 'rgb(255,196,61)'
+};
