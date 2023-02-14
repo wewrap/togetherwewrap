c2395ea23c36a8804b63aac89e2a668b321f60ef
Fahad Khan <fakha@MacBook-Pro-2.attlocal.net>
2023-02-13 23:21:17 -0600

    cleanup app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,9 @@
-import axios from "axios";
-import { useState } from "react";
 import {LoginForm} from './Components/loginForm'
-const serverURL = 'http://localhost:8000'
 
 function App() {
-  const [message, setMessage] = useState<string>("");
-
-  const resetMessage = () => {
-    setMessage("")
-  }
-
-  const handleClick = async () => {
-    const isDisplayingMessage = !!message
-    if (isDisplayingMessage) {
-      resetMessage()
-      return
-    };
-
-    try {
-      const res = await axios.get(`${serverURL}/api`)
-      const serverMessage = res.data.data
-      setMessage(serverMessage)
-    } catch (error) {
-      //error has type 'unknown'. My fix: assert error as any.
-      console.error("Error connecting to backend:", (error as any).message)
-      alert(`Error connecting to backend: ${(error as any).message}`)
-    }
-  }
   return (
     <div className="App">
-      <h2>{message ?? ""}</h2>
       <LoginForm />
-      {/* <button onClick={handleClick}>click me!</button> */}
     </div>
   );
 }
