faf10ad6e907e9e1202e99ba18c38d23f670d332
Fahad Khan <121646456+fahadakhan2@users.noreply.github.com>
2023-02-17 00:21:43 -0600

    [T40] Build Login Page (#13)
    * stash
    
    * Implemented backend logic for login
    
    * changed axios version
    
    * Revert axios version
    
    * Changed axios request from a promise to asyc/await syntax
    
    * onChange functions pulled out and defined separately
    
    * removed setUsername and setPassword as arguments to the handleChange variables
    
    * fixed some word differences. Keeping underscores only between multiple words
    
    * moved auth middleware logic into its own folder
    
    * changed backend logic to check for username and password using our prisma user model
    
    * removed unecessary p tags and put the information into there correlating label tags
    
    * imported prisma to login_auth file rather than creating another instance of the prisma client which could cause some database issues.
    
    * removed unecessary p tags and put the information into there correlating label tags
    
    * cleanup app
    
    * remove frontend files
    
    * Revert "remove frontend files"
    
    This reverts commit 43b6fe0b91b0bcfedd89474ca10c02fc23f81d91.
    
    * rm newline
    
    * Changed all instances of 'username' to email, fixed capitalization for labels, and added 'Forgot password' p tag.
    
    * Fixed white bottom on login page by regulating the height and margin for the parent structures in index.css
    
    * Completed css for login form
    
    * Created task and comment reminder for temp href tags for signup and forgot password link redirect
    
    * Removed extra lines
    
    * Added css to parent bodies to fix white bottom css issue
    
    * Added missing env variables
    
    * Added TODO T105
    
    * applied stylings to the inputs with their class names instead of the word 'input' itself.
    
    ---------
    
    Co-authored-by: Fahad Khan <fakha@MacBook-Pro-2.attlocal.net>

--- backend/routes/loginAuth.ts
+++ backend/routes/loginAuth.ts
@@ -7,13 +7,13 @@ import crypto from 'crypto';
 
 const db = prisma;
 
-loginAuthRouter.get('/login/password',
+loginAuthRouter.get('/password',
     function (req, res, next) {
         res.render('login');
     });
 
-loginAuthRouter.post('/login/password',
-    passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
+loginAuthRouter.post('/password',
+    passport.authenticate('local', { failureMessage: true }),
     function (req, res) {
         console.log(req.body)
         res.status(200).send("we logged in")
