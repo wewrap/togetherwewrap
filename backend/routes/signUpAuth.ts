977aff41266c45f0e7344746ad34dfd4919841be
jesshe004 <101468113+jesshe004@users.noreply.github.com>
2023-02-17 19:37:34 -0500

    deleted files and comments

--- backend/routes/signUpAuth.ts
+++ backend/routes/signUpAuth.ts
@@ -1,13 +1,11 @@
 import * as dotenv from 'dotenv';
 import path from 'path';
 dotenv.config({path: path.resolve(__dirname, "../.env")});
-import app from '../utils/app';
 import cors from 'cors';
-import prisma from '../utils/db';
-import { PrismaClient } from '@prisma/client';
+import prisma from '../utils/prismaClient';
 import morgan from 'morgan';
 import express from 'express';
-
+import app from '../app';
 import passport from 'passport';
 import {Strategy as LocalStrategy} from 'passport-local';
 import crypto from 'crypto'
@@ -29,19 +27,10 @@ app.post('/signup', async function(req, res, next) {
                 firstName: req.body.firstName,
                 lastName: req.body.lastName, 
                 password: hashedPassword.toString("base64"),
-                salt: salt //fix salt ??? how to best generate salt using crypto 
+                salt 
             },
         })
         console.log(user); //redirect to the login page 
     })
     res.send(200);
 });
-
-//error caused by emails should be unique but using same email fix error 
-// error states, styling forms, fix routes, deicide what needs to be sent back to the client (what's the next step) (reroute to a specific page), 
-
-// app.post('/signup', 
-// passport.authenticate('local', {failureRedirect: '/signup', failureMessage: true }),
-//  function(req, res) {
-//     console.log(req.body)
-//  });
