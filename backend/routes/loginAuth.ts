3a1017ca854b3f3761887e9a219d89b62d1e7262
Fahad Khan <121646456+fahadakhan2@users.noreply.github.com>
2023-02-19 12:35:27 -0600

    T82: Catch the error response for incorrect password entered during login (#20)
    * fixed login routes
    
    * Changed failure message when password does not match.
    
    * error to be returned out in the front end
    
    * passing in an object for passport.use telling passport to look at email and password of looking at user, pass
    
    * Manually posting our errors instead of passport
    
    * updated css for loginForm to accomodate the errors now being displayed upon unauthorized login
    
    * minor tweaks and change to the loginForm css to accomadate height change when error pops up.

--- backend/routes/loginAuth.ts
+++ backend/routes/loginAuth.ts
@@ -1,10 +1,8 @@
 import express from "express";
-const loginAuthRouter = express.Router()
-import { Strategy as LocalStrategy } from 'passport-local';
 import passport from 'passport'
 import prisma from '../utils/prismaClient'
-import crypto from 'crypto';
 
+const loginAuthRouter = express.Router()
 const db = prisma;
 
 loginAuthRouter.get('/password',
@@ -12,35 +10,16 @@ loginAuthRouter.get('/password',
         res.render('login');
     });
 
-loginAuthRouter.post('/password',
-    passport.authenticate('local', { failureMessage: true }),
-    function (req, res) {
-        console.log(req.body)
-        res.status(200).send("we logged in")
-    });
-
-passport.use(new LocalStrategy(async (email: string, password: string, done: Function) => {
-    try {
-        const user = await db.user.findUnique({
-            where: {
-                email
-            },
-        });
-
-        if (!user || !user.salt) {
-            return done(null, false, { message: 'Incorrect email or password.' });
-        }
-
-        const hashedPassword = crypto.pbkdf2Sync(password, user.salt, 310000, 32, 'sha256').toString('hex');
-        if (user.password !== hashedPassword) {
-            return done(null, false, { message: 'Incorrect email or password.' });
-        }
-
-        return done(null, user);
-
-    } catch (error) {
-        done(error);
-    }
-}));
+loginAuthRouter.post("/password", function (req, res, next) {
+    passport.authenticate("local", function (err, user, info) {
+      if (err) {
+        return res.status(401).send('Email or password did not match. Please try again.')
+      }
+      if (!user) {
+        return res.status(401).send('Email or password did not match. Please try again.');
+      }
+      req.login(user, next);
+    })(req, res, next);
+  });
 
 export default loginAuthRouter
