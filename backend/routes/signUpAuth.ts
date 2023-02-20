db8343ee63936606664fb246dab89117df272cb9
jesshe004 <101468113+jesshe004@users.noreply.github.com>
2023-02-20 00:11:54 -0500

    merged w main

--- backend/routes/signUpAuth.ts
+++ backend/routes/signUpAuth.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({path: path.resolve(__dirname, "../.env")});
+dotenv.config();
 import cors from 'cors';
 import prisma from '../utils/prismaClient';
 import morgan from 'morgan';
