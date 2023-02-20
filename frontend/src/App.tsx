f65bebf909371346c8ce78a6eafb54768bb03f2b
kevin <kevinvong0129@gmail.com>
2023-02-20 11:45:03 -0700

    fix imports in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
-import {LoginForm} from './components/loginForm';
-import {SignUp} from './components/signup';
+import {LoginForm} from './Components/loginForm';
+import {SignUp} from './Components/signup';
 import './index.css';
-import axios from "axios";
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-const serverURL = 'http://localhost:8000';
 function App() {
   return (
 <div> 
