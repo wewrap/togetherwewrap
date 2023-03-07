1053328c6585dde7c790f061c68c9c6ddc332990
jess <101468113+jesshe004@users.noreply.github.com>
2023-03-06 19:39:44 -0500

    added money urls & extended a profile model (#32)

---
+++ backend/prisma/migrations/20230303001811_extend/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Profile` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `photoUrl` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `Profile_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD CONSTRAINT `Profile_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
