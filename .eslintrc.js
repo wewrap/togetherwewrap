753a5977df551796e751fad7338d40a42ec90eb1
kevin <kevinvong0129@gmail.com>
2023-03-21 19:56:25 -0700

    move .eslintrc back to root

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    eqeqeq: ['error', 'always']
+  }
+}
