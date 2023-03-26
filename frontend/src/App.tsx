02648af39590685547a44ae506595fe012b4e854
kevin <kevinvong0129@gmail.com>
2023-03-26 15:28:21 -0700

    add create context for user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,26 @@
-import {LoginForm} from './components/loginForm';
-import {SignUp} from './components/signup';
-import { TempLandingPage } from './components/tempLandingPage';
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Contacts } from './components/contacts';
-function App() {
+import { LoginForm } from './components/loginForm'
+import { SignUp } from './components/signup'
+import { TempLandingPage } from './components/tempLandingPage'
+import './index.css'
+import { Routes, Route } from 'react-router-dom'
+import { Contacts } from './components/contacts'
+import { UserContext } from './components/UserContext'
+import { useState } from 'react'
+
+function App (): JSX.Element {
+  const userState = useState(null)
   return (
-<div> 
-    <Routes> 
-      <Route path = "/login" element = {<LoginForm/>}> </Route>
-    </Routes>
-    <Routes> 
-      <Route path = "/signup" element={<SignUp/>}>  </Route>
-    </Routes>
-    <Routes>
-      <Route path = "/tempLandingPage" element={<TempLandingPage/>}> </Route>
-    </Routes>
-    <Routes>
-      <Route path = "/contactPage" element={<Contacts/>}> </Route>
-    </Routes>
-</div>
-  );
+    <div>
+      <Routes>
+        <UserContext.Provider value={userState}>
+          <Route path="/login" element={<LoginForm />}> </Route>
+          <Route path="/signup" element={<SignUp />}>  </Route>
+          <Route path="/tempLandingPage" element={<TempLandingPage />}> </Route>
+          <Route path="/contactPage" element={<Contacts />}> </Route>
+        </UserContext.Provider>
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
