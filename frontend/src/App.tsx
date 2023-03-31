091d4c1fce1aaf94eab46bf7ea3de443c9ad0ab9
ntd6039 <97009566+ntd6039@users.noreply.github.com>
2023-03-30 23:24:06 -0400

    Add calendar to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,29 @@
-import {LoginForm} from './components/loginForm';
-import {SignUp} from './components/signup';
+import { LoginForm } from './components/loginForm';
+import { SignUp } from './components/signup';
 import { TempLandingPage } from './components/tempLandingPage';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Contacts } from './components/contacts';
+import Calendar from './components/calendar_frontend/calendar';
 function App() {
   return (
-<div> 
-    <Routes> 
-      <Route path = "/login" element = {<LoginForm/>}> </Route>
-    </Routes>
-    <Routes> 
-      <Route path = "/signup" element={<SignUp/>}>  </Route>
-    </Routes>
-    <Routes>
-      <Route path = "/tempLandingPage" element={<TempLandingPage/>}> </Route>
-    </Routes>
-    <Routes>
-      <Route path = "/contactPage" element={<Contacts/>}> </Route>
-    </Routes>
-</div>
+    <div>
+      <Routes>
+        <Route path="/login" element={<LoginForm />}> </Route>
+      </Routes>
+      <Routes>
+        <Route path="/signup" element={<SignUp />}>  </Route>
+      </Routes>
+      <Routes>
+        <Route path="/tempLandingPage" element={<TempLandingPage />}> </Route>
+      </Routes>
+      <Routes>
+        <Route path="/contactPage" element={<Contacts />}> </Route>
+      </Routes>
+      <Routes>
+        <Route path="/calendar" element={<Calendar />}></Route>
+      </Routes>
+    </div>
   );
 }
 
