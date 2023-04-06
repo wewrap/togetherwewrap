3fda198018d1ee70ce0002fc0f24c8c1be6dd0d9
kevin <kevinvong0129@gmail.com>
2023-04-05 20:07:26 -0700

    add hamburger icons and amount colllected box

--- frontend/src/components/Plan.tsx
+++ frontend/src/components/Plan.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import './Plan.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Plan = (): JSX.Element => {
   return (
     <div className='plan-container'>
-      <div className='plan-hamburger'>hamburger</div>
-      <div className='money-container'>
-        <h4>Amount Collected</h4>
-        <button>+</button>
+      <div className='top-container'>
+        <button className='burger-btn'><FontAwesomeIcon icon={icon({ name: 'bars' })} /></button>
+        <h1 className='special-person-header'>Special Person: Bob Jones</h1>
+        <div className='collected-container'>
+          <h4>Amount Collected: 4124</h4>
+          <button> <FontAwesomeIcon icon={icon({ name: 'plus' })} /></button>
+        </div>
       </div>
-      <h1 className='special-person-header'>Special Person: Bob Jones</h1>
       <section className='plan-section'>
         <div id='plan-description'>
           <button>edit</button>
           <article>
-           <p id='description'>Bob is clebrating his 25th birthday, lets buy him some gifts</p>
-           <div id='special-date'>special date</div>
-           <h5>wish list</h5>
-           <ul>
-             <li>thing 1</li>
-             <li>thing 2</li>
-             <li>thing 3</li>
-           </ul>
+            <p id='description'>Bob is clebrating his 25th birthday, lets buy him some gifts</p>
+            <div id='special-date'>special date</div>
+            <h5>wish list</h5>
+            <ul>
+              <li>thing 1</li>
+              <li>thing 2</li>
+              <li>thing 3</li>
+            </ul>
           </article>
         </div>
 
         <div id='idea-list'>
-          <h4>Idea list</h4>
+          <h4 id='idea-list-heading'>Idea list</h4>
           <button>add button</button>
           <ul>
             <li>metallica shirt</li>
@@ -34,15 +39,15 @@ export const Plan = (): JSX.Element => {
             <li>movie tickets</li>
           </ul>
         </div>
-        <div id='friend-list'>
-          <h3>Pledges</h3>
-          <ul>
+        <div id='pledge-list'>
+          <h3 id='pledge'>Pledges</h3>
+          <ul className='pledge-accepted'>
             <li>leader: John</li>
             <li>claire</li>
             <li>Jake</li>
           </ul>
-          <h3>Pending invites: </h3>
-          <ul>
+          <h3 id="pending-invites">Pending invites: </h3>
+          <ul className='pledge-pending'>
             <li>Nick</li>
             <li>Ana</li>
             <li>emily</li>
