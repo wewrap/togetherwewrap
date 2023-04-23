fb4113958df23b84965df702bb4e8f1fd7876a17
kevin <kevinvong0129@gmail.com>
2023-04-23 16:09:17 -0700

    refactor fetchPlanData

--- backend/service/plan.ts
+++ backend/service/plan.ts
@@ -27,42 +27,45 @@ export default class PlanService {
 
       if (planMembers === null || planMembers === undefined) throw new Error('Unable to fetch plan members')
 
-      const planData: GeneralPlanData = {
-        description: plan.description,
-        specialDate: plan.endDate,
-        specialPerson: null,
-        members: {
-          planLeader: null,
-          acceptedMembers: [],
-          pendingMembers: [],
-          deniedMembers: []
-        }
-      }
-
-      for (const member of planMembers) {
-        const userWithPlanMembership = member.user
-        if (member.role === Role.SPECIAL_PERSON) planData.specialPerson = userWithPlanMembership
-        else if (member.role === Role.PLAN_LEADER) planData.members.planLeader = userWithPlanMembership
-        else if (member.role === Role.FRIEND) {
-          switch (member.inviteStatus) {
-            case InviteStatus.DENY:
-              planData.members.deniedMembers.push(userWithPlanMembership)
-              break;
-            case InviteStatus.ACCEPTED:
-              planData.members.acceptedMembers.push(userWithPlanMembership)
-              break;
-            case InviteStatus.INVITED:
-              planData.members.pendingMembers.push(userWithPlanMembership)
-              break;
-            default:
-              throw new Error('asd')
-          }
-        }
-      }
-      return planData
+      return PlanService.populatePlanDataObjectHelper(plan, planMembers)
     } catch (err) {
       console.error(`Service failure: failed to fetch plan ${err}`)
       return null
     }
+  }
+
+  private static populatePlanDataObjectHelper(plan: Plan, planMembers: any): GeneralPlanData {
+    const planData: GeneralPlanData = {
+      description: plan.description,
+      specialDate: plan.endDate,
+      specialPerson: null,
+      members: {
+        planLeader: null,
+        acceptedMembers: [],
+        pendingMembers: [],
+        deniedMembers: []
+      }
+    }
+    for (const member of planMembers) {
+      const userWithPlanMembership = member.user
+      if (member.role === Role.SPECIAL_PERSON) planData.specialPerson = userWithPlanMembership
+      else if (member.role === Role.PLAN_LEADER) planData.members.planLeader = userWithPlanMembership
+      else if (member.role === Role.FRIEND) {
+        switch (member.inviteStatus) {
+          case InviteStatus.DENY:
+            planData.members.deniedMembers.push(userWithPlanMembership)
+            break;
+          case InviteStatus.ACCEPTED:
+            planData.members.acceptedMembers.push(userWithPlanMembership)
+            break;
+          case InviteStatus.INVITED:
+            planData.members.pendingMembers.push(userWithPlanMembership)
+            break;
+          default:
+            throw new Error('asd')
+        }
+      }
+    }
+    return planData
   }
 }
