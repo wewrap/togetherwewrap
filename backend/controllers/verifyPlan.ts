62b34d7a95bea36621a64123d380d4ed28beee3b
kevin <kevinvong0129@gmail.com>
2023-05-21 21:58:39 -0700

    remove verify plan invite stuff

--- backend/controllers/verifyPlan.ts
+++
@@ -1,7 +0,0 @@
-import { type NextFunction } from 'express';
-
-export default class PlanInviteController {
-  static async verifyPlanInvite(req: any, res: any, next: NextFunction): Promise<void> {
-
-  }
-}
--- backend/routes/verifyPlanInvite.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-const verifyPlanInviterouter = express.Router()
-
-verifyPlanInviterouter.get('/:id', )
-
-export default verifyPlanInviterouter;
