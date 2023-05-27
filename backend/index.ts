7f3b8f104d4f1bc6f517ef71a4c87286b665c93f
kevin <kevinvong0129@gmail.com>
2023-05-27 09:01:14 -0700

    add node env in config file

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,4 @@
-/* Index ts is responsible for connecting the app to local port*/
+/* Index ts is responsible for connecting the app to local port */
 
 import app from './app';
 import http from 'http';
@@ -8,5 +8,5 @@ const server = http.createServer(app)
 const port = 8000;
 
 server.listen(port, () => {
-    console.log(`Server started: http://localhost:${port}/`);
+  console.log(`Server started: http://localhost:${port}/`);
 });
--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -9,9 +9,22 @@ const facebookAppSecret = process.env.FACEBOOK_APP_SECRET as string
 const facebookCallBackURL = process.env.FACEBOOK_CALLBACK_URL as string
 const googleCallBackURL = process.env.GOOGLE_CALLBACK_URL as string
 
+process.env.NODE_ENV = process.env.NODE_ENV ?? 'development'
+
+let envConfig: Record<string, unknown>;
+if (process.env.NODE_ENV === 'development') {
+  envConfig = {
+    port: 3000
+  }
+} else if (process.env.NODE_ENV === 'production') {
+  envConfig = {
+    port: process.env.PORT
+  }
+}
+
 export {
   secretcode, googleClientID,
   googleClientSecret, facebookAppSecret,
   facebookClientID, facebookCallBackURL,
-  googleCallBackURL
+  googleCallBackURL, envConfig
 }
