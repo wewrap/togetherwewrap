b270ce72309b91a295483a231986e26916b1112b
kdata12 <106122976+kdata12@users.noreply.github.com>
2023-05-31 17:29:57 -0700

    node env port (#66)
    * add node env in config file
    
    * change redirect url
    
    * fix port number
    
    * fix

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,3 @@
-import * as dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
@@ -24,7 +23,6 @@ import contactCreatorRouter from './routes/contactCreator'
 import logoutRouter from './routes/logout'
 import inviteContactsRouter from './routes/inviteContactsRouter'
 
-dotenv.config()
 const GoogleStrategy = googleStrategy.Strategy
 const FacebookStrategy = facebookStrategy.Strategy
 const app = express()
@@ -37,7 +35,7 @@ app.use(morgan('dev'))
 
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000'
+  // origin: 'http://localhost:3000'
 }));
 app.use(express.json());
 app.use(passport.initialize())
--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
 
 const secretcode = process.env.SESSION_SECRET as string
 const googleClientID = process.env.GOOGLE_CLIENT_ID as string
@@ -9,9 +11,18 @@ const facebookAppSecret = process.env.FACEBOOK_APP_SECRET as string
 const facebookCallBackURL = process.env.FACEBOOK_CALLBACK_URL as string
 const googleCallBackURL = process.env.GOOGLE_CALLBACK_URL as string
 
+process.env.NODE_ENV = process.env.NODE_ENV ?? 'development'
+
+let port: number | string | undefined;
+if (process.env.NODE_ENV === 'development') {
+  port = 3000
+} else if (process.env.NODE_ENV === 'production') {
+  port = process.env.PORT
+}
+
 export {
   secretcode, googleClientID,
   googleClientSecret, facebookAppSecret,
   facebookClientID, facebookCallBackURL,
-  googleCallBackURL
+  googleCallBackURL, port
 }
