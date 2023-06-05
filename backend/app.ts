ea88bb1d276c4e402c7dd312a96832ea81863078
kdata12 <106122976+kdata12@users.noreply.github.com>
2023-06-04 20:34:40 -0700

    T140: Plan member list frontend (#68)
    * add modal
    
    * add search bar and tag
    
    * plan home changes
    
    * fix plan button
    
    * quick logout and auth fix
    
    * add types and error message
    
    * add types
    
    * clean up and add clear button
    
    * add filteredSearchByAlreadySelected
    
    * quick fixes
    
    * pr fixes
    
    * rm old searchbar and tag files
    
    * pr hygiene
    
    * add plan id to post request
    
    * temp fix on planForm.tsx
    
    * pr fixes
    
    * backend cleanup
    
    * add member list mock data
    
    * add axios get memberlist
    
    * fix route mistake

--- backend/app.ts
+++ backend/app.ts
@@ -35,7 +35,7 @@ app.use(morgan('dev'))
 
 app.use(cors({
   credentials: true,
-  // origin: 'http://localhost:3000'
+  origin: 'http://localhost:3000'
 }));
 app.use(express.json());
 app.use(passport.initialize())
