cbaf4f95587d0977f48741f785d79d673ad0e8f2
kevin <kevinvong0129@gmail.com>
2023-06-17 12:23:32 -0700

    add local_db in env

--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -11,11 +11,19 @@ const facebookAppSecret = process.env.FACEBOOK_APP_SECRET as string
 const facebookCallBackURL = process.env.FACEBOOK_CALLBACK_URL as string
 const googleCallBackURL = process.env.GOOGLE_CALLBACK_URL as string
 
+const LOCAL_DB_USERNAME = process.env.LOCAL_DB_USERNAME
+const LOCAL_DB_HOST = process.env.LOCAL_DB_HOST
+const LOCAL_DB_DBNAME = process.env.LOCAL_DB_DBNAME
+const LOCAL_DB_PASS = process.env.LOCAL_DB_PASS
+
+const LOCAL_DATABASE_URL = `mysql://${LOCAL_DB_USERNAME}:${LOCAL_DB_PASS}@${LOCAL_DB_HOST}/${LOCAL_DB_DBNAME}`;
+
 process.env.NODE_ENV = process.env.NODE_ENV ?? 'development'
 
 let port: number | string | undefined;
 if (process.env.NODE_ENV === 'development') {
   port = 8000
+  process.env.DATABASE_URL = LOCAL_DATABASE_URL
 } else if (process.env.NODE_ENV === 'production') {
   port = process.env.PORT
 }
